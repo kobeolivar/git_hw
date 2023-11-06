9a6260ff4e2a2c4c1a01a26abf267a8348d8fe3b
kobeolivar <kolivar1030@gmail.com>
2023-11-06 18:13:27 -0500

    My second unittest

--- test2.py
+++ test2.py
@@ -1,12 +1,19 @@
 import unittest
-from simplemath import generate_addition_problem
+from simplemath import generate_multiplication_problem, check_answer
 
-class TestMathProblem(unittest.TestCase):
-    def test_wrong_answer(self):
-        """Test the response to a wrong answer."""
-        correct_answer = generate_addition_problem()
-        user_answer = correct_answer + 1  # Intentionally wrong
-        self.assertNotEqual(user_answer, correct_answer, "The user's answer should be wrong.")
+class TestMultiplicationProblem(unittest.TestCase):
+    def test_generate_multiplication_problem(self):
+        """Test that the multiplication problem generates integers and products correctly."""
+        result = generate_multiplication_problem()
+        self.assertIsInstance(result, int, "The result should be an integer.")
+        self.assertTrue(1 <= result <= 10000, "Product should be between 1 and 10000.")
+
+    def test_check_answer_multiplication(self):
+        """Test the checking of the correct answer for a multiplication problem."""
+        self.assertIsNone(check_answer(20, 20), "The check_answer function should return None for a correct answer.")
+        with self.assertRaises(ValueError):  # Checking for ValueError now
+            check_answer(20, 30)
+
 
 if __name__ == '__main__':
     unittest.main()
