1a0bf7f369a7cf40ac5edaee96c896ffb8bcf7ff
kobeolivar <kolivar1030@gmail.com>
2023-11-06 18:13:04 -0500

    Solving problems for addition and multiplication

--- simplemath.py
+++ simplemath.py
@@ -7,13 +7,37 @@ def generate_addition_problem():
     print(f"What is {num1} + {num2}?")
     return sum
 
-def main():
-    answer = generate_addition_problem()
-    user_answer = int(input("Your answer: "))
-    if user_answer == answer:
+def generate_multiplication_problem():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    multiply = num1 * num2
+    print(f"What is {num1} * {num2}?")
+    return multiply
+
+def get_user_answer():
+    try:
+        return int(input("Your answer: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+        return get_user_answer()  # Recursive call to prompt again
+
+def check_answer(user_answer, correct_answer):
+    if user_answer == correct_answer:
         print("Correct!")
     else:
-        print(f"Wrong. The correct answer is {answer}.")
+        print(f"Wrong. The correct answer is {correct_answer}.")
+        raise ValueError("Incorrect answer")
+
+def main():
+    # Addition problem
+    answer = generate_addition_problem()
+    user_answer = get_user_answer()
+    check_answer(user_answer, answer)
+    
+    # Multiplication problem
+    answer = generate_multiplication_problem()
+    user_answer = get_user_answer()
+    check_answer(user_answer, answer)
 
 if __name__ == "__main__":
     main()
